8e3382295007c5e88501f81513392789e3c429ac
Jairo Gomes <evaristojairo12@gmail.com>
2021-03-09 10:51:35 -0300

    add cors in project

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const router = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(logger('dev'));
 app.use('/api/v1', router);
 
